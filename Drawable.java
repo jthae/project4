8bf12e91f9acb3213bd096ea042e2b03aece8d9c
jthae <40273807+jthae@users.noreply.github.com>
2019-04-16 19:24:37 -0500

    Implement Drawable

---
+++ Drawable.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+/**
+ * @author jayciethaemert
+ * 
+ * Abstract class for draw method for shapes.
+ *
+ */
+public abstract class Drawable {
+	
+	protected abstract void draw(Graphics graphics);
+
+}
